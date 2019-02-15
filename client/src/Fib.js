ce0b3e3f644d3bc1a713fb2d53fde24dfd57a309
Leonhard Wolfmayr <leonhard@wolfmayr.net>
2019-02-15 16:42:02 +0100

    .

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-      return this.state.seenIndexes.map(({ number }) => number).join(', ');    
+      /* return this.state.seenIndexes.map(({ number }) => number).join(', ');    */
+      return '1';
   }
 
   renderValues() {
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
-#docker build -t lwolfmayr/multi-client:latest -t lwolfmayr/multi-client:$SHA -f ./client/Dockerfile ./client
-#docker build -t lwolfmayr/multi-server:latest -t lwolfmayr/multi-server:$SHA -f ./server/Dockerfile ./server
-#docker build -t lwolfmayr/multi-worker:latest -t lwolfmayr/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t lwolfmayr/multi-client:latest -t lwolfmayr/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t lwolfmayr/multi-server:latest -t lwolfmayr/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t lwolfmayr/multi-worker:latest -t lwolfmayr/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-#docker push lwolfmayr/multi-client:latest
-#docker push lwolfmayr/multi-server:latest
-#docker push lwolfmayr/multi-worker:latest
+docker push lwolfmayr/multi-client:latest
+docker push lwolfmayr/multi-server:latest
+docker push lwolfmayr/multi-worker:latest
 
-#docker push lwolfmayr/multi-client:$SHA
-#docker push lwolfmayr/multi-server:$SHA
-#docker push lwolfmayr/multi-worker:$SHA
+docker push lwolfmayr/multi-client:$SHA
+docker push lwolfmayr/multi-server:$SHA
+docker push lwolfmayr/multi-worker:$SHA
 
-# apply stephengrider for testing
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:latest
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:latest
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:latest
+kubectl set image deployments/server-deployment server=lwolfmayr/multi-server:latest
+kubectl set image deployments/client-deployment client=lwolfmayr/multi-client:latest
+kubectl set image deployments/worker-deployment worker=lwolfmayr/multi-worker:latest
