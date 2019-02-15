dbb5acf96962b798748ae3417606fa05a13be538
Leonhard Wolfmayr <leonhard@wolfmayr.net>
2019-02-15 10:11:39 +0100

    change

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    if(typeof this.state.seenIndexes === "function") {
-      return this.state.seenIndexes.map(({ number }) => number).join(', ');
-    }
+      return this.state.seenIndexes.map(({ number }) => number).join(', ');    
   }
 
   renderValues() {
